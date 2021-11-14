e69153c6ea420b44756159381a316e6ffe9745aa
Shoki Yokomizo <curin030204@yahoo.co.jp>
2021-11-14 16:29:02 +0900

    Hello Worldの実行

--- Hello World/main.go
+++ Hello World/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+// 複数の場合は丸括弧でまとめることもできる
+import (
+	"fmt"
+	"time"
+)
 
 // Hello World
 
+// パッケージの作成→関数の作成→必要ならパッケージのインポート
+// パッケージの宣言は必ず1つ
+
+/*
+複数行のコメントはこのように書く
+*/
+
 func main() {
-	// 改行付きで出力する関数
+	// 渡したデータを改行付きで出力する関数
 	fmt.Println("Hello World")
+	// 時間の表示
+	fmt.Println(time.Now())
 }
