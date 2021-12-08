d792d61f0b52c8bb0bc26f83dc96cee0be1874cc
Shoki Yokomizo <curin030204@yahoo.co.jp>
2021-12-09 07:29:05 +0900

    interface型について

--- variable/interface/main.go
+++ variable/interface/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// interface型は{}まで含めて型の名前になる
+	// 他の型との互換性がある
+	// 初期値は nil
+	// 演算の対象としては使えない
+	// 全ての型を表すことができる
+	var x interface{}
+	fmt.Println(x)
+
+	// 数値も入る
+	x = 1
+	fmt.Println(x)
+	x = 3.14
+	fmt.Println(x)
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
+
+	x = 2
+	fmt.Println(x + 3)
 }
